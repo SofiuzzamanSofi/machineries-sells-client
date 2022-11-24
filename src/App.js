4c5765dbc4957477942d17dafd6f75f93cdd7eba
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-11-24 18:09:37 +0600

    install tailwind & daisyui

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ function App() {
   return (
     <div className="">
       Machineries-Sells
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+      <div>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row">
+            <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
+            <div>
+              <h1 className="text-5xl font-bold">Box Office News!</h1>
+              <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
